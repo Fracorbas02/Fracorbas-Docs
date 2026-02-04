8f2b44c2a28142aeb0a8b9d757a77726aeef0598
fracorbas <bastien.bonora@gmail.com>
2026-02-04 20:02:29 +0100

    chore: removing useless files

--- load.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# dans un premier temps on lance npm pour monter tout ce que l'on a :
-npm run build
-
-# Ensuite, il faut que l'on copie tout ce que l'on a comme fichiers
-sudo cp -r build/* /var/www/html
